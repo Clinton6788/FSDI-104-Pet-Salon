7bf87c97c35daead6cc20ac587d6f9d3f51e2945
Clinton6788 <clinton.g.hockenberry@gmail.com>
2024-09-05 13:06:31 -0400

    backup before image changes

--- scripts/register.js
+++ scripts/register.js
@@ -8,22 +8,29 @@ let pet1={
     name:"Frank",
     age:1,
     gender:"Male",
+    service:"Grooming",
     breed:"Bernedoodle"
 }
 let pet2={
     name:"Luna",
     age:3,
     gender:"Female",
+    service:"Nail Trim",
     breed:"Golden Mountain"
+}
+let pet3={
+    name:"Chloe",
+    age:6,
+    gender:"Female",
+    service:"Bath",
+    breed:"Shorthair Cat"
 }
 
 document.write(pet1.name);
 document.write(pet2.name);
 
 //Push pets into array
-pets.push(pet1,pet2)
-
-
+pets.push(pet1,pet2,pet3)
 
 function displayNames(){
     console.log("Total pets = " + pets.length);
