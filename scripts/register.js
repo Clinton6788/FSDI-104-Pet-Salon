19ad46e6a1a7fee81e449f65b138995c6671ee0e
Clinton6788 <clinton.g.hockenberry@gmail.com>
2024-09-05 21:42:38 -0400

    Added pet boxes, media sizing

--- scripts/register.js
+++ scripts/register.js
@@ -9,31 +9,69 @@ let pet1={
     age:1,
     gender:"Male",
     service:"Grooming",
-    breed:"Bernedoodle"
-}
+    breed:"Bernedoodle",
+    photo: "./img/Frank.jpg"
+};
 let pet2={
     name:"Luna",
     age:3,
     gender:"Female",
     service:"Nail Trim",
-    breed:"Golden Mountain"
-}
+    breed:"Golden Mountain",
+    photo:"./img/Luna.jpg"
+};
 let pet3={
     name:"Chloe",
-    age:6,
+    age:7,
     gender:"Female",
     service:"Bath",
-    breed:"Shorthair Cat"
+    breed:"Shorthair Cat",
+    photo:"./img/Chloe.jpg"
+};
+let pet4={
+    name:"Freyja",
+    age:3,
+    gender:"Female",
+    service:"Attitude Adjustment",
+    breed:"Cat",
+    photo:"./img/Freyja.jpg"
 }
 
-document.write(pet1.name);
-document.write(pet2.name);
-
 //Push pets into array
-pets.push(pet1,pet2,pet3)
+pets.push(pet1,pet2,pet3,pet4)
 
 function displayNames(){
     console.log("Total pets = " + pets.length);
     console.log(pets[0].name)
     console.log(pets[1].name);
+}
+console.log(pets.length)
+for(let i=0;i<pets.length;i++){
+    document.getElementById("petBoxes").innerHTML+=`
+    <div class="pet-box">
+        <div class="center-img">
+            <img class="pet-img" src="${pets[i].photo}" alt="">
+        </div>
+        <h3>${pets[i].name}, ${pets[i].age} year(s)</h3>
+        <div class="details-grid">
+            <p>Gender:</p>
+            <p>${pets[i].gender}</p>
+            <p>Breed:</p>
+            <p>${pets[i].breed}</p>
+            <p>Service:</p>
+            <p>${pets[i].service}</p>
+        </div>    
+    </div>`
+
+    document.getElementById("numberRegistered").innerHTML="Currently Registered: " + pets.length
+}
+
+function averageAges(){
+    let sum=0;
+    for(let i=0;i<pets.length;i++){
+        sum += pets[i].age;
+    }
+    let average = sum/pets.length;
+    console.log(average);
+    
 }
